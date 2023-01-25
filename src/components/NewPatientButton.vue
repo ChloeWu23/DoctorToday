a4e14ba7b53dd66b591fca8123fe9da1883d0706
Chen, Yutong <yutong.chen22@imperial.ac.uk>
2023-01-25 17:08:55 +0000

    Added new patient registration button for redirection to the page

--- src/components/NewPatientButton.vue
+++ src/components/NewPatientButton.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-white border-2 border-sky-700 rounded-lg w-1/6 shadow-lg">
-        <RouterLink to="/appointment">
+        <RouterLink to="/newpatientregister">
             <span class="max-w-sm justify-end">
-                <p class="font-bold text-sky-700 text-center p-6">Request an appointment slot</p>
+                <p class="font-bold text-sky-700 text-center p-6">Please only register after your appointment is confirmed by the clinic.</p>
             </span>
         </RouterLink>
     </div>
