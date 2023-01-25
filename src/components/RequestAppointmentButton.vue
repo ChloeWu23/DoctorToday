d118321cd42ad8a6b7a3e079e439680a25560600
Chen, Yutong <yutong.chen22@imperial.ac.uk>
2023-01-25 16:58:18 +0000

    Added Request Appointment Button; Removed NewPatientRegister, to be moved to Views

---
+++ src/components/RequestAppointmentButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-white border-2 border-sky-700 rounded-lg w-1/6 shadow-lg">
+        <RouterLink to="/appointment">
+            <span class="max-w-sm justify-end">
+                <p class="font-bold text-sky-700 text-center p-6">Request an appointment slot</p>
+            </span>
+        </RouterLink>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
