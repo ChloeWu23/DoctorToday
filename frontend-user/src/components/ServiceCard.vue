4f8c104af594f513d55662ea87d0c4e851e9caca
yc1422 <yc1422@ic.ac.uk>
2023-01-27 00:09:14 +0000

    move userside files to frontend-user folder

---
+++ frontend-user/src/components/ServiceCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="grid gap-10 md:grid-cols-3 divide-x grid-auto-flow: column">
+        <div v-for="service in fullServices" :key="service.name">
+            <div class="bg-sky-100 border-sky-700 border-b-4 min-h-full p-4 drop-shadow-md ">
+                <p class="
+                font-bold text-sky-600 
+                underline underline-offset-4 decoration-sky-700 ">{{ service.name }}
+                </p>
+                <p class="my-2">include:</p>
+                <span v-for="subCatetory in service.children" :key="service.children.name">
+                    <li><a :href="subCatetory.path">{{ subCatetory.name }}</a></li>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import services from '../assets/config.json'
+
+export default {
+    data() {
+        return {
+            fullServices: services.priceList
+        }
+    }
+}</script>
---
+++ frontend-user/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>PLACEHOLDER FOR HOME PAGE
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {}
+  }
+}
+</script>
+
