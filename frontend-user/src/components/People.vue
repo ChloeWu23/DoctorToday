b8bb6503f5eaa6717a869b267cc814b9f5dc0dd7
Shuyao Zhang <shuyao.zhang18@imperial.ac.uk>
2023-02-06 11:35:56 +0000

    link people.vue with backend data

--- frontend-user/src/components/People.vue
+++ frontend-user/src/components/People.vue
@@ -2,12 +2,12 @@
     <div class ="grid_12">
 		<ul id="people">
             <li v-for="(item,index) in peopleList" :key="item.id">
-				<p style="float: left; width: 100px; margin-right: 20px; margin-bottom: 20px;"> Space for Image</p>
+				<p src="" style="float: left; width: 100px; margin-right: 20px; margin-bottom: 20px;"> Space for Image </p>
                     <div sytle="float:left;">
-                        <h3>{{item.name}}</h3>
-					    <p><strong>{{item.title}}</strong></p>
-					    <p class="italic">{{item.profile}}</p>
-		 			    <p style="margin-left: 120px;">{{item.description}}</p>
+                        <p class="text-xl m-1">{{item.name}}</p>
+					    <p class="text-sm font-bold">{{item.title}}</p>
+					    <p class="text-sm italic m-3">{{item.profile}}</p>
+		 			    <p class="text-sm mb-4" style="margin-left: 120px;">{{item.description}}</p>
 		 			</div>
 		 		<div style="clear: both"></div>
 			</li>
@@ -21,16 +21,13 @@ import DataPeople from '@/dataRoutes/DataPeople'
 export default {
     data () {
         return {
-            peopleList: [
-                {name: "name1", title: "title1", profile: "profile1", description: "description1"},
-                {name: "name2", title: "title2", profile: "profile2", description: "description2"}
-            ]
+            peopleList: []
         }
     },
     mounted() {
-        DataPeople.get().then(rec => {
-            console.log(res.data);
-            peopleList = res.data;
+        DataPeople.get()
+        .then(res => {
+            this.peopleList = res.data;
         })
         .catch(err => {
             console.log("Error: cannot retrieve people data");
