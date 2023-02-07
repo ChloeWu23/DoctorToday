5c5a0925ac556efb2ac742eebf4451d89e711327
yc1422 <yc1422@ic.ac.uk>
2023-02-07 23:32:32 +0000

    update trustpilot link in navbar

--- frontend-user/src/components/NavBar.vue
+++ frontend-user/src/components/NavBar.vue
@@ -8,7 +8,10 @@
                 <li class="md:mx-4 md:my-0 my-6 text-white relative before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 
                 before:bg-white before:block before:hover:scale-x-100 before:scale-x-0
                 before:transition before:ease-in-out before:duration-300">
-                    <RouterLink :to="tab.path" class="text-white">{{ tab.label }}</RouterLink>
+                    <span v-if="tab.name === 'review'"><a @click="openRedirection()">{{ tab.label }}</a></span>
+                    <span v-else>
+                        <RouterLink :to="tab.path" class="text-white">{{ tab.label }}</RouterLink>
+                    </span>
                 </li>
             </div>
         </ul>
@@ -43,7 +46,7 @@ export default {
                 },
                 {
                     path: "/bookingAppointment",
-                    name:"bookingAppointment",
+                    name: "bookingAppointment",
                     label: "Appointments"
                 },
                 {
@@ -53,7 +56,7 @@ export default {
                 },
                 {
                     path: "/people",
-                    name:"people",
+                    name: "people",
                     label: "People"
                 },
 
@@ -67,13 +70,18 @@ export default {
                     name: "about",
                     label: "About Us"
                 },
-                             
+
             ]
         }
     },
     data() {
         return {
             showMenu: false
+        }
+    },
+    methods: {
+        openRedirection() {
+            window.open('https://uk.trustpilot.com/review/doctortoday.co.uk?utm_medium=trustbox&utm_source=MicroReviewCount')
         }
     }
 
