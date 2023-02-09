8ea69025c9456eae6b63a73b4652677adb4b99f2
Chloe Wu <Chloe.wu22@imperial.ac.uk>
2023-02-09 23:03:48 +0000

    for merge master into this branch

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "127.0.0.1",
     USER: "root",
-    PASSWORD: "",
-    DB: "vuevue",
+    PASSWORD: "karena_WD23",
+    DB: "vue",
       port:3306,
     dialect: "mysql",
     pool: {
