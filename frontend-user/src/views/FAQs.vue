291a916e7456f01375ffaf86e5bf72d9dc2f7dc1
Chloe Wu <Chloe.wu22@imperial.ac.uk>
2023-02-09 01:25:40 +0000

    change format of FAQ

--- frontend-user/src/views/FAQs.vue
+++ frontend-user/src/views/FAQs.vue
@@ -5,43 +5,33 @@
         <div><br></div>
 	</div> 
     
-    <div class = "text-left m-0">
-        <div v-for="(item,index) in QuestionList" :key="index" class="mb-2">
-            <button @click="item.showAnswer = !item.showAnswer"
-                class="text-left w-full py-2 text-gray-700 font-medium bg-gray-200 hover:bg-gray-300">
+      <div class = "container">
+        <ol>
+        <li v-for="(item,index) in QuestionList" :key="index" class="mb-4">
+            <li @click="item.showAnswer = !item.showAnswer"
+                class="text-left w-full py-1 text-black italic font-medium  hover:bg-sky-200">
         {{ item.question }}
-            </button>
+            </li>
             <transition name="slide-down">
-                <div v-if="item.showAnswer"
-                    class="p-4 bg-gray-100 border border-gray-300 rounded-lg">
+                <p v-if="item.showAnswer"
+                    class="py-2 p-4 bg-white font-bold border border-gray-300 rounded-lg">
                         {{ item.answer }}
-                 </div>
+                </p>
             </transition>
-        </div>
-        <!--
-    <ol style="list-style-type: decimal">
-        <li v-for = "(item,index) in QuestionList" :key = "index"  @click="showAnswer">
-            <p class = "text-left text-lg font-bold m-2" v-if="showAnswer">
-                {{ item.question }}
-            </p>
-            <p class = "text-left text-medium italic m-2">
-                {{ item.answer }}
-            </p>
         </li>
-    </ol>
--->
-    </div>
+        </ol>
+      </div>
 </template>
 
 <script>
-import { Flag } from '@element-plus/icons'
+
 import DataFAQ from '../dataRoutes/DataFAQ'
 
 export default {
     data () {
         return {
             QuestionList:[],
-            flag: false
+            
         }
     },
     /*
