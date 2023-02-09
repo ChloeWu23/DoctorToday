152a609313d416a2660571ae123545ecfe059fe1
Shuyao Zhang <shuyao.zhang18@imperial.ac.uk>
2023-02-09 17:36:01 +0000

    decorated FAQ and Appointments

--- frontend-user/src/views/BookingAppointment.vue
+++ frontend-user/src/views/BookingAppointment.vue
@@ -1,13 +1,13 @@
 <template>
-    <div><br></div>
-    <div>
+    <div class="px-4 py-4">
         <h1 class = "text-gray-700 font-bold text-xl font-sans">Booking Requests<br></h1>
-        <p class = "text-lg">Appointments can be requested online via our <RouterLink class = "text-sky-700 font-extrabold" to="Appointment"> online booking system.</RouterLink> 
+        <p class = "text-base">Appointments can be requested online via our <RouterLink class = "text-sky-700 font-extrabold underline underline-offset-4" to="Appointment"> online booking system.</RouterLink> 
         You will need to wait for one of our friendly staff to email you back with a confimation before your appoitnment is booked.
-        <b class = "text-lg text-black font-bold">Please check your junk mail for confirmation emails.</b> </p>
+        <b class = "text-base text-black font-bold">Please check your junk mail for confirmation emails.</b> </p>
         <div><br></div>
-        <div class = "text-lg">
-            <p>Alternatively, appointments can be booked by telephone on 020 7433 1444 or <a class = "text-sky-700 font-extrabold" href="https://wa.me/447879635767"> WhatsApp</a> . </p>
+
+        <div class = "text-base">
+            <p>Alternatively, appointments can be booked by telephone on 020 7433 1444 or <a class = "text-sky-700 font-extrabold underline underline-offset-4" href="https://wa.me/447879635767"> WhatsApp</a> . </p>
                 
                 If you are not sure which services you will require, our friendly and helpful reception staff will 
                 be happy to discuss your needs.
@@ -18,15 +18,16 @@
             </p>
         </div>
         <div><b><br></b></div>
+
         <h1 class = "text-gray-700  font-bold text-xl font-sans">Walk-in Appointments</h1>
-        <p class="text-lg">Our walk in service is <b>temporarily suspended</b> due to 
+        <p class="text-base">Our walk in service is <b>temporarily suspended</b> due to 
             social distancing - we do not currently have a waiting room and 
             ask all patients to please call or email to book an appointment.
             <br></p>
 
          <div><b><br></b></div>
         <h1 class = "text-gray-700  font-bold text-xl font-sans">Family members, friends and chaperones</h1>
-        <p class="text-lg">Patients are welcome to attend with a family member or friend. At your specific request, 
+        <p class="text-bae">Patients are welcome to attend with a family member or friend. At your specific request, 
         they may sit in on the consultation. For females undergoing intimate examinations or tests 
         by male clinicians, it is our policy that a female chaperone is always present, for your peace 
         of mind as well as ours so that probity is assured. This chaperone may be one of the female reception staff when a nurse 
@@ -35,7 +36,7 @@
         
         <div><b><br></b></div>
         <h1 class = "text-gray-700  font-bold text-xl font-sans">Children</h1>
-        <p class = "text-lg">Please note that children under 16 years of age will not be seen unless accompanied by a 
+        <p class = "text-base">Please note that children under 16 years of age will not be seen unless accompanied by a 
             parent or other person with parental responsibility. However, with consent of both parties, 
             a child under 16 may be seen privately for non-intimate examinations by a same-sex or 
             female clinician.
@@ -43,16 +44,11 @@
 
         <div><b><br></b></div>
         <h1 class = "text-gray-700  font-bold text-xl font-sans">Register Online</h1>
-        <div class = "text-lg">You can save time at your appointment by filling out this
-             <RouterLink class = "text-sky-700 font-extrabold" to="NewPatientRegister">online patient registration</RouterLink> 
+        <div class = "text-base">You can save time at your appointment by filling out this
+             <RouterLink class = "text-sky-700 font-extrabold underline underline-offset-4" to="NewPatientRegister">online patient registration</RouterLink> 
              form prior to your arrival.
         </div>
-            <div><br></div><div><br></div><div><br></div>	
-    
     </div>
-    
-
-  
 </template>
 
 <script>
--- frontend-user/src/views/FAQs.vue
+++ frontend-user/src/views/FAQs.vue
@@ -1,38 +1,47 @@
 <template>
     
-    <div class = "text-left text-2xl">
+    <div class = "h-full shadow-md m-1 md:m-8">
         <div><br></div>
-		<h1 class="text-sky-700">Frequently Asked Questions</h1>
-        <div><br></div>
-	</div> 
-    
-    <div class = "min-w-full float-left">
-        <ol>
-            <li v-for="(item,index) in QuestionList" :key="index" class="mb-4">
-                <button  
-                    class="text-left w-full py-1 text-black text-lg font-extrabold  hover:bg-gray-200"
-                    @click="item.showAnswer = !item.showAnswer">
-                      <span class = "font-bold inline-flex">{{ index +1 }}. &nbsp;</span>
-                      {{ item.question }}    
-            </button>
-            
-            <transition name="slide-fade">
-                <p v-if="item.showAnswer"
-                    class="py-2 p-4 bg-white font-bold text-base ">
-                        {{ item.answer }}
-                </p>
-            </transition>
-            <hr class="text-grey-300 border-1.5">
-            </li>
-            
-        </ol>
+		<h1 class="text-2xl text-sky-700 px-6 pb-5">Frequently Asked Questions</h1>
+
+        <div class="">
+            <details class="open:bg-gray-100 duration-300" v-for="(item,index) in QuestionList" :key="index">
+                <summary
+                    class="px-4 py-3 text-lg cursor-pointer hover:bg-gray-100 border-b-2 flex justify-between items-center w-full text-sm md:text-base">
+                    <div class="p-2 text-sky-700">
+                        <div>{{index+1}}. {{ item.question }}</div>
+                    </div>
+                </summary>
+                <div class="bg-white p-6 text-base text-justify">
+                    {{ item.answer }}
+                </div>
+            </details>
+        </div>
     </div>
 
 </template>
 
 <script>
 import DataFAQ from '../dataRoutes/DataFAQ'
-
+//<hr class="text-grey-300 border-1.5 m-0 p-0">
+/*
+<div class = "">
+            <ol>
+                <li v-for="(item,index) in QuestionList" :key="index" class="">
+                    <button  
+                    class="text-left w-full py-1 text-black text-lg font-semibold hover:bg-gray-200 border-b-0 border-gray-200"
+                    @click="item.showAnswer = !item.showAnswer">
+                        <span class = "font-bold inline-flex">{{ index +1 }}. &nbsp;</span>
+                      {{ item.question }}    
+                    </button>
+            
+                    <transition name="slide-fade">
+                        <p v-if="item.showAnswer" class="py-2 p-4 bg-white text-base ">{{ item.answer }}</p>
+                    </transition>
+                </li>
+            </ol>
+        </div>
+        */
 export default {
     data () {
         return {
@@ -50,3 +59,23 @@ export default {
     }
 }
 </script>
+
+<style>
+details>summary {
+    list-style: none;
+}
+
+details summary::-webkit-details-marker {
+    display: none;
+}
+
+details summary::after {
+    content: '+';
+    font-size: 2em;
+    color: slategrey
+}
+
+details[open] summary::after {
+    content: "-";
+}
+</style>
