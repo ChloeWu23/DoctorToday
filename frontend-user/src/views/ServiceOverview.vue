aafe3a8a45e8af60274aef5a88669eee24b90664
yc1422 <yc1422@ic.ac.uk>
2023-02-13 23:28:15 +0000

    change bg colour for service overview

--- frontend-user/src/views/ServiceOverview.vue
+++ frontend-user/src/views/ServiceOverview.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="m-10 grid gap-10 md:grid-cols-2 lg:grid-cols-3 divide-x grid-auto-flow: column">
         <div v-for="service in fullServices" :key="service.serviceName.replace(/\s+/g, '-').toLowerCase()">
-            <div class="hover:bg-[#143B71] delay-100 text-[#143B71] hover:text-white bg-contain bg-no-repeat bg-right-bottom p-4 shadow-md h-full border-t-4 border-[#143B71]"
+            <div class="hover:bg-[#143B71] delay-100 text-[#143B71] hover:text-white bg-contain bg-no-repeat bg-right-bottom bg-[#143B71]/10 p-4 shadow-md h-full border-t-4 border-[#143B71]"
                 :style="{ 'background-image': 'url(' + require('@/assets/' + service.serviceName.replace(/\s+/g, '-').toLowerCase() + '.png') + ')' }">
                 <RouterLink :to="'services/' + service.serviceName.replace(/\s+/g, '-').toLowerCase()">
                     <p class="font-bold">
