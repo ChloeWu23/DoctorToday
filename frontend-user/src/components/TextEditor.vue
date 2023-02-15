f18795eb676fbf8eaf9c66e61e15f8500be20e53
yc1422 <yc1422@ic.ac.uk>
2023-02-15 21:51:06 +0000

    tidy up text editor

--- frontend-user/src/components/TextEditor.vue
+++ frontend-user/src/components/TextEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <QuillEditor id="textEditor" theme="snow" toolbar="essential" contentType="html" v-model:content="textContent" @update:content="checkText"/>
+    <QuillEditor id="textEditor" theme="snow" toolbar="essential" contentType="html" v-model:content="textContent" />
     <div class="p-4"></div>
     <button class="bg-sky-600 text-white p-2 rounded" @click="sendToServer">Save</button>
   </div>
@@ -17,13 +17,17 @@ export default {
   data() {
     return {
       textContent: "",
+      dataBaseTable: ""
     };
   },
   methods: {
-    checkText($event) {
-        console.log($event);
-    },
+    // removed from QuillEditor tag @update:content="checkText"
+    // checkText($event) {
+    //     console.log($event);
+    // },
     sendToServer(){
+      console.log(this.textContent);
+      //save html data to the table specified in dataBaseTable, which should be a parameter passed from parent component
       return true;
     }
   }
