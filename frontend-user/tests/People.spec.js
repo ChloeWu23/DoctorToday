30c317626e45cf5b5d2afaa014040fd06b803329
Shuyao Zhang <shuyao.zhang18@imperial.ac.uk>
2023-02-23 14:41:33 +0000

    tests before and after clicking

--- frontend-user/tests/People.spec.js
+++ frontend-user/tests/People.spec.js
@@ -13,14 +13,15 @@ describe('People', () => {
         name: "Test Name",
         title: "Test Title",
         profile: "Test Profile",
-        description: "Test Description"
+        description: "Test Description",
+        image: "@/assets/booking/booking.png"
     }]
 
     it('render proper texts for name, title and profile', () => {
         const wrapper = mount(People, {
             data () {
                 return {
-                    peopleList:
+                    peopleList: testPeopleList
                 }
             }
         })
@@ -28,8 +29,16 @@ describe('People', () => {
         expect(wrapper.text()).toContain(testPeopleList[0].name)
         expect(wrapper.text()).toContain(testPeopleList[0].title)
         expect(wrapper.text()).toContain(testPeopleList[0].profile)
-
     })
+
+    /*
+    it('render proper people image', async () => {
+        const wrapper = mount(People)
+        const img = await wrapper.get('img')
+
+        expect(img.attributes('src')).toBe(testPeopleList[0].image)
+    })
+    */
 
     it('not show description when button is no clicked', () => {
         const wrapper = mount(People, {
@@ -53,7 +62,9 @@ describe('People', () => {
         })
 
         await wrapper.find("button").trigger("click")
-
+        const text = wrapper.find("div");
+        
+        expect(text.isVisible()).toBe(true);
         expect(wrapper.text()).toContain(testPeopleList[0].description)
     })
 })
