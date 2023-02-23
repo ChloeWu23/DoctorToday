863af87e8f6e066766d7ef9079fe7491e7439819
Shuyao Zhang <shuyao.zhang18@imperial.ac.uk>
2023-02-23 01:19:03 +0000

    vitest configurated

---
+++ frontend-user/tests/People.spec.js
@@ -0,0 +1,9 @@
+import People from '../src/views/People.vue'
+
+test('should work as expected', () => {
+    expect(Math.sqrt(4)).toBe(2)
+});
+
+test('People', () => {
+    console.log(People);
+})
