3f662cb7d30994ee43a4a4b40907996417b2083a
yqcui <15833977395@163.com>
2023-03-04 22:00:50 +0000

    udpate backend package

--- backend/routes/peopleRoutes.js
+++ backend/routes/peopleRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const multer = require('multer')
-const multerS3 = require('multer-s3');
+// const multerS3 = require('multer-s3');
 const path = require('path');
 
 const { Sequelize } = require("../app/models");
