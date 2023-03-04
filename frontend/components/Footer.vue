a6bd633494dcdbe5bc03122b155b0d27314f9c95
yc1422 <yc1422@ic.ac.uk>
2023-03-04 15:22:12 +0000

    add terms page

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -10,10 +10,9 @@
             <p><a href="https://www.cqc.org.uk/location/1-117106339?referer=widget4">Registered by the Care Quality Commission</a></p>
             <!-- links to be added -->
             <p><a href="">Site Developers</a> |
-                <a href="">Terms and Conditions</a> |
+                <a href="https://doctor-today-app.herokuapp.com/about/Terms">Terms and Conditions</a> |
                 <a href="https://doctor-today-app.herokuapp.com/about/FAQs">Privacy Policy</a>
             </p>
-
         </div>
     </div>
 </template>
