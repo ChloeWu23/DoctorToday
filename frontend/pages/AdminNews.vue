082b355db73ec7f33a32e0aa2d29da893a32c222
Anyan <az321@ic.ac.uk>
2023-03-05 20:46:34 +0000

    [feat]modify AdminNews, news do not have EDIT

--- frontend/pages/AdminNews.vue
+++ frontend/pages/AdminNews.vue
@@ -39,13 +39,15 @@
                         </td>
 
                         <td class="flex items-center px-6 py-4 space-x-3">
-                            <!-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> -->
-                            <button @click="deleteNews(news.news_id)"
-                                class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
-                            <button @click="swapNewsDown(news.news_id)"
-                                class="font-medium text-green-600 dark:text-green-500 hover:underline">Down</button>
                             <button @click="swapNewsUp(news.news_id)"
-                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Up</button>
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline w-6 hover:bg-gray-200 rounded">
+                                <img src="../assets/admin_portal/icon-up-arrow.svg" alt="Icon" class="mr-2" />
+                            </button>
+
+                            <button @click="deleteNews(news.news_id)"
+                                class="font-medium text-red-600 dark:text-red-500 hover:underline w-7 hover:bg-gray-200 rounded">
+                                <img src="../assets/admin_portal/icon-delete.svg" alt="Icon" class="mr-2" />
+                            </button>
                         </td>
                     </tr>
                 </tbody>
@@ -53,8 +55,10 @@
 
         </div>
 
-        <AddNewsDialog v-model="dialogFormVisible" v-if="dialogFormVisible" @refresh-callback="refreshServiceView" />
-        <AddNewsDialog />
+        <AddNewsDialog 
+            v-model="dialogFormVisible" 
+            v-if="dialogFormVisible" 
+            @refresh-callback="refreshServiceView" />
     </div>
 </template>
 
