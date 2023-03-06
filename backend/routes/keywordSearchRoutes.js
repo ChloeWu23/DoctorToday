187b80fbfbd8b5a286c223fa8c9e3d22a6846ec3
Chen, Yutong <yutong.chen22@imperial.ac.uk>
2023-03-06 13:39:55 +0000

    fix search api

--- backend/routes/keywordSearchRoutes.js
+++ backend/routes/keywordSearchRoutes.js
@@ -6,18 +6,12 @@ const router = require("express").Router();
 router.get('/', (req, res) => {
     console.log("get request triggered");
     urls = [
-        { id: 0, url: "http://localhost:3000/bookingAppointment", title: "Appointments" },
-        { id: 1, url: "http://localhost:3000/about/FindUs", title: "Find Us" },
-        // { id: 2, url: "http://localhost:3000/about/People", title: "People" },
-        { id: 3, url: "http://localhost:3000/about/FAQs", title: "FAQs" },
-        { id: 4, url: "http://localhost:3000/travelGuides", title: "Travel Guides" },
-        // { id: 5, url: "http://localhost:3000/services", title: "Service & Prices" }
-        // { id: 0, url: "https://doctor-today-app.herokuapp.com/bookingAppointment", title: "Appointments" },
-        // { id: 1, url: "https://doctor-today-app.herokuapp.com/about/FindUs", title: "Find Us" },
-        // { id: 2, url: "https://doctor-today-app.herokuapp.com/about/People", title: "People" },
-        // { id: 3, url: "https://doctor-today-app.herokuapp.com/about/FAQs", title: "FAQs" },
-        // { id: 4, url: "https://doctor-today-app.herokuapp.com/travelGuides", title: "Travel Guides" },
-        // { id: 5, url: "https://doctor-today-app.herokuapp.com/serviceSummary", title: "Service & Prices" },
+        { id: 0, url: "https://doctor-today-app.herokuapp.com/bookingAppointment", title: "Appointments" },
+        { id: 1, url: "https://doctor-today-app.herokuapp.com/about/FindUs", title: "Find Us" },
+        { id: 2, url: "https://doctor-today-app.herokuapp.com/about/People", title: "People" },
+        { id: 3, url: "https://doctor-today-app.herokuapp.com/about/FAQs", title: "FAQs" },
+        { id: 4, url: "https://doctor-today-app.herokuapp.com/travelGuides", title: "Travel Guides" },
+        { id: 5, url: "https://doctor-today-app.herokuapp.com/services", title: "Service & Prices" },
         // { id: 6, url: "https://doctor-today-app.herokuapp.com/services/gp-consultations", title: "GP Consultations" },
         // { id: 7, url: "https://doctor-today-app.herokuapp.com/services/occupational-health", title: "Occupational Health" },
         // { id: 8, url: "https://doctor-today-app.herokuapp.com/services/sexual-health", title: "Sexual Health" },
@@ -35,24 +29,26 @@ router.get('/', (req, res) => {
 })
 
 async function search(urls) {
-    const requests = urls.map((urlItem) => axios.get(urlItem.url));
-
-    const htmls = Promise.all(requests).then((responses) => {
-        let htmls = []
-        for (let i = 0; i < responses.length; i++) {
-            console.log(urls[i].title)
-            try {
-                const $ = cheerio.load(responses[i].data)
-                const $html = $('body').text()
-                htmls.push({ id: i, title: urls[i].title, url: urls[i].url, content: $html })
-            }
-            catch (err) {
-                console.log(urls[i].url)
+  const htmls = Promise.allSettled(urls.map((urlItem) => axios.get(urlItem.url)))
+    .then((results) => {
+        let htmls = [];
+        for (let i = 0; i < results.length; i++) {
+            if (results[i].status === "fulfilled") {
+                const $ = cheerio.load(results[i].value.data);
+                const $html = $("body").text();
+                htmls.push({
+                    id: urls[i].id,
+                    title: urls[i].title,
+                    url: urls[i].url,
+                    content: $html
+                });
+            } else {
+                console.log("issue with url: " + urls[i].url);
             }
         }
         return htmls;
     });
     return htmls;
-};
+}
 
 module.exports = router;
