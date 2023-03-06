327c8912f9e3df1ba2364182540214dba36077df
Zhao, Anyan <anyan.zhao22@imperial.ac.uk>
2023-03-06 22:10:57 +0000

    fix pageIndex.add

--- backend/routes/pageIndexRoutes.js
+++ backend/routes/pageIndexRoutes.js
@@ -55,6 +55,9 @@ router.post("/", async(req, res) => {
         })
     });
 
+    if (id_max === null || id_max === undefined) {
+        id_max = -1;
+    }
     
     var newItem = {
         id: id_max + 1,
