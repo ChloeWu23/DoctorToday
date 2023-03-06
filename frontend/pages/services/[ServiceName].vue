7c1ceb4475d1339457ccc274acff024417b2e073
yc1422 <yc1422@ic.ac.uk>
2023-03-06 18:42:44 +0000

    service details working

--- frontend/pages/services/[ServiceName].vue
+++ frontend/pages/services/[ServiceName].vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="h-full md:w-[48rem] lg:w-[60rem] shadow-md mx-auto lg:max-screen-lg">
+    <div class="h-full md:w-[48rem] lg:w-[60rem] shadow-md mx-auto lg:max-screen-lg" v-if="serviceDetails">
         <SembleButton v-if=sembleFormCode :sembleForm=sembleFormCode />
         <div class="w-full">
-            <details class="open:bg-gray-100 duration-300 w-full" v-for="item in this.serviceDetails">
+            <details class="open:bg-gray-100 duration-300 w-full" v-for="item in serviceDetails">
                 <summary
                     class="px-5 py-3 text-lg cursor-pointer hover:bg-gray-100 border border-2 flex justify-between items-center w-full text-sm md:text-base">
                     <div class="p-2 text-sky-700 w-3/4">
@@ -23,36 +23,39 @@
 export default {
     data() {
         return {
-            sembleFormCode: ''
         }
     },
-    async setup() {
+    setup() {
         const route = useRoute()
         console.log(route.params.ServiceName)
         try {
-            const { data: fullServices, pending, error, refresh } = await useAsyncData(
-                () => $fetch("https://doctor-today-back.herokuapp.com/service")
-            )
-            // console.log(fullServices.value)
-
             let serviceId = ref()
             let sembleFormCode = ref()
-            for (let i = 0; i < fullServices.value.length; i++) {
-                if (fullServices.value[i].serviceName.replace(/\s+/g, '-').toLowerCase() === route.params.ServiceName) {
-                    serviceId.value = fullServices.value[i].bind_id
-                    sembleFormCode.value = fullServices.value[i].appointment_iframe
-                    break;
-                }
-            }
-
-            const { data: serviceDetails } = await useAsyncData(
-                () => $fetch("https://doctor-today-back.herokuapp.com/service/" + serviceId.value, {
-                    immediate: false,  // prevent the request from firing immediately
-                    watch: [serviceId] // watch reactive sources to auto-refresh
+            let serviceDetails = ref([])
+            const { data: fullServices } = useFetch(
+                "https://doctor-today-back.herokuapp.com/service")
+                .then((res) => {
+                    // console.log(res.data.value)
+                    for (let i = 0; i < res.data.value.length; i++) {
+                        if (res.data.value[i].serviceName.replace(/\s+/g, '-').toLowerCase() === route.params.ServiceName) {
+                            console.log(res.data.value[i].bind_id)
+                            serviceId.value = res.data.value[i].bind_id
+                            sembleFormCode.value = res.data.value[i].appointment_iframe
+                            break;
+                        }
+                    }
+                    return serviceId
+                }).then((id) => {
+                    // console.log(id)
+                    $fetch("https://doctor-today-back.herokuapp.com/service/" + id.value)
+                        .then((res) => {
+                            res.forEach((item) => {
+                                console.log(item)
+                                serviceDetails.value.push(item)
+                            })
+                        })
                 })
-            )
-
-            return { serviceDetails, sembleFormCode }
+            return { sembleFormCode, serviceId, serviceDetails }
         } catch (err) {
             console.log(err)
             console.log(route.params.ServiceName)
