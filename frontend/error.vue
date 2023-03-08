628320f16363ff77e374cffc48bc582a370b2177
yc1422 <yc1422@ic.ac.uk>
2023-03-08 11:31:59 +0000

    add mobile responsiveness

--- frontend/error.vue
+++ frontend/error.vue
@@ -1,8 +1,8 @@
 <template>
     <Header></Header>
-    <div class="mx-auto w-96 flex flex-col items-center">
-        <p class="align-center">Page Not Found, click below to back to homepage</p>
-        <div class="grid grid-cols-2 m-8 mb-20 gap-10">
+    <div class="mx-auto flex flex-col items-center container">
+        <p class="align-center">This page you are trying to access is not available now.</p>
+        <div class="grid md:grid-cols-2 m-8 mb-20 gap-6 md:gap-10">
             <button @click="redirectToHome" class="w-40 h-12 bg-sky-700/90 text-white rounded-lg">Home Page</button>
             <button @click="bookAppointment" class="w-40 h-12 bg-sky-700/90 text-white rounded-lg">Book
                 Appointment</button>
