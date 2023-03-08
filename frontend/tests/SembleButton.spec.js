dd4a8c79e6de94cfd9bca0242b6286a228d6a894
yc1422 <yc1422@ic.ac.uk>
2023-03-08 00:37:30 +0000

    fix semblebutton test

--- frontend/tests/SembleButton.spec.js
+++ frontend/tests/SembleButton.spec.js
@@ -1,9 +1,18 @@
 import { mount } from '@vue/test-utils'
 import SembleButton from '../components/SembleButton.vue'
+import RequestAppointmentModal from '../components/RequestAppointmentModal.vue'
 
 describe('Semble Button', () => {
   test('clicking the cross closes the modal ', () => {
-    const wrapper = mount(SembleButton)
+    const data = { sembleForm: '<iframe src="">Mock</iframe>' }
+    const wrapper = mount(SembleButton, {
+      propsData: {
+        data,
+      },
+      stubs: ['RequestAppointmentModal'], // stub the child component
+    })
+
+    const childWrapper = wrapper.findComponent(RequestAppointmentModal)
     const button = wrapper.find('button')
     const initial = wrapper.vm.showModal;
     button.trigger('click')
