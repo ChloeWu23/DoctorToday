ac8e58279081d6c92d4d08f0d9287444716ff8fc
Anyan <az321@ic.ac.uk>
2023-03-08 13:45:01 +0000

    [feat] add/delete dependencies in ServiceRoutes

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -6,14 +6,8 @@ const ServiceOverviews = db.ServiceOverviews;
 const SubService = db.SubService;
 const Op = db.Sequelize.Op;
 
-
+// get all service data
 router.get('/', (req, res) => {
-  // const fs = require('fs');
-  // const data = JSON.parse(
-  //   fs.readFileSync('./config-data/data.json')
-  // );
-  // console.log(data);
-
   ServiceOverviews.findAll({
     order: ["service_cat_id"]
   })
@@ -29,6 +23,26 @@ router.get('/', (req, res) => {
   });
 });
 
+// get one serviceSummary data
+router.get('/getOne/:service_cat_id', (req, res) => {
+  ServiceOverviews.findOne({
+    where: {
+      service_cat_id: req.params.service_cat_id
+    }
+  })
+  .then(data => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.status(200).json(data);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: 
+        err.message || "Some error occurred while retriving one service data"
+    });
+  });
+});
+
+// get all sub-service with cat_id
 router.get('/:cat_id', (req, res) => {
   SubService.findAll({
     where: {
